9a055f9d8167d9b836f77d17f2f7061ee2390521
Gladys Valentin <gvalentin@unm.edu>
2017-02-18 22:56:24 -0700

    Added variables and infoWindows

--- js/map_m4.js
+++ js/map_m4.js
@@ -4,11 +4,16 @@ function initialize() {
 	var flamenco = new google.maps.LatLng(18.328375, -65.316861)
 	var sunbay = new google.maps.LatLng(18.097135, -65.461280)
 	var crashboat = new google.maps.LatLng(18.458587, -67.164352)
+	var escanbron = new google.maps.LatLng(18.467103, -66.625923)
+	var marchiquita = new google.maps.LatLng(18.492285, -66.090163)
+	var gilligan = new google.maps.LatLng(17.942551, -66.873859)
 	
 	var myOptions = {
 		zoom: 9,
 		center: prcenter,
 		mapTypeId: google.maps.MapTypeId.HYBRID,
+		
+		// Googel style done at https://mapstyle.withgoogle.com/
 		styles: [
   {
     "featureType": "administrative.land_parcel",
@@ -129,23 +134,19 @@ function initialize() {
 	var map = new google.maps.Map(
 		document.getElementById("map_canvas"), 
 		myOptions);
-		
-		
-		
-		
+	
+	// Playuela infoWindow content, infoWindow, marker, click function
+				
 	var playuelaString = 
-		'</div>'+
-        '<div id="bodyContent">'+
+		'<div id="bodyContent">'+
 		'<p><b>La Playuela Beach</b>, '+
         'also known as Playa Sucia, is located ' +
         'in the municipality of Cabo Rojo, Puerto Rico, near the '+
         'Cabo Rojo Lighthouse and Las Salinas. '+
-        '</div>'+
         '</div>';						
 		
 	var playuelaInfoWindow = new google.maps.InfoWindow({
 		content: playuelaString
-		
 	});
 	
 	var playuelaMarker = new google.maps.Marker({
@@ -158,9 +159,16 @@ function initialize() {
 		playuelaInfoWindow.open(map,playuelaMarker);
 	});
 	
+	// Flamenco infoWindow content, infoWindow, marker, click function
 	
-
-	
+	var flamencoString = 
+		'<div id="bodyContent">'+
+		'<p><b>Flamenco Beach</b> '+
+        'is known for its beautiful white sands ' +
+        'and clear waters. It is located in the island-municipality '+
+        'of Culebra, Puerto Rico. '+
+        '</div>';
+		
 	var flamencoMarker = new google.maps.Marker({
 		position: flamenco,
 		title:"Flamenco Beach, Culebra"
@@ -168,14 +176,21 @@ function initialize() {
 	flamencoMarker.setMap(map);
 	
 	var flamencoInfoWindow = new google.maps.InfoWindow({
-		content: flamencoInfoWindow
+		content: flamencoString
 	});
 	google.maps.event.addListener(flamencoMarker, 'click', function() {
 		flamencoInfoWindow.open(map,flamencoMarker);
 	});
 	
+	// Sun Bay infoWindow content, infoWindow, marker, click function
 	
-	
+	var sunbayString = 
+		'<div id="bodyContent">'+
+		'<p><b>Sun Bay Beach</b> '+
+        'is located on the south coast of the island-municipality of Vieques, ' +
+        'walking distance from the Puerto Mosquito Bioluminicent Bay.'+
+        '</div>';
+		
 	var sunbayMarker = new google.maps.Marker({
 		position: sunbay,
 		title:"Sun Bay Beach, Vieques"
@@ -183,14 +198,21 @@ function initialize() {
 	sunbayMarker.setMap(map);
 	
 	var sunbayInfoWindow = new google.maps.InfoWindow({
-		content: sunbayInfoWindow
+		content: sunbayString
 	});
 	google.maps.event.addListener(sunbayMarker, 'click', function() {
 		sunbayInfoWindow.open(map,sunbayMarker);
 	});
 	
+	// Crash Boat infoWindow content, infoWindow, marker, click function
 	
-	
+	var crashboatString = 
+		'<div id="bodyContent"> '+
+		'<p><b>Crash Boat Beach</b> '+
+        'gets its name from the remnants of a ship wreck near by. '+
+		'It is located in the Aguadilla municipality, west coast of Puerto Rico. '+
+        '</div>';
+		
 	var crashboatMarker = new google.maps.Marker({
 		position: crashboat,
 		title:"Crash Boat Beach, Aguadilla"
@@ -198,10 +220,73 @@ function initialize() {
 	crashboatMarker.setMap(map);
 	
 	var crashboatInfoWindow = new google.maps.InfoWindow({
-		content: crashboatInfoWindow
+		content: crashboatString
 	});
 	google.maps.event.addListener(crashboatMarker, 'click', function() {
 		crashboatInfoWindow.open(map,crashboatMarker);
+	});
+	
+	// Escanbron infoWindow content, infoWindow, marker, click function
+	
+	var escanbronString = 
+		'<div id="bodyContent"> '+
+		'<p><b>Escanbron Beach</b> '+
+        'is located in the historic Isleta de San Juan, Puerto Rico. '+
+        '</div>';
+		
+	var escanbronMarker = new google.maps.Marker({
+		position: escanbron,
+		title:"Escanbrón Beach, San Juan"
+		});
+	escanbronMarker.setMap(map);
+	
+	var escanbronInfoWindow = new google.maps.InfoWindow({
+		content: escanbronString
+	});
+	google.maps.event.addListener(escanbronMarker, 'click', function() {
+		escanbronInfoWindow.open(map,escanbronMarker);
+	});
+	
+	// Mar Chiquita infoWindow content, infoWindow, marker, click function
+	
+	var marchiquitaString = 
+		'<div id="bodyContent"> '+
+		'<p><b>Mar Chiquita Beach</b> '+
+        '_____________________________________ '+
+        '</div>';
+		
+	var marchiquitaMarker = new google.maps.Marker({
+		position: marchiquita,
+		title:"Mar Chiquita Beach, Manati"
+		});
+	escanbronMarker.setMap(map);
+	
+	var marchiquitaInfoWindow = new google.maps.InfoWindow({
+		content: marchiquitaString
+	});
+	google.maps.event.addListener(marchiquitaMarker, 'click', function() {
+		marchiquitaInfoWindow.open(map,marchiquitaMarker);
+	});
+
+	// Gilligan infoWindow content, infoWindow, marker, click function
+	
+	var gilliganString =
+		'<div id="bodyContent"> '+
+		'<p><b>Gilligan Island</b> '+
+        '_____________________________________ '+
+        '</div>';
+		
+	var gilliganMarker = new google.maps.Marker({
+		position: guilligan,
+		title:"Gilligan Island, Guanica"
+		});
+	gilliganMarker.setMap(map);
+	
+	var gilliganInfoWindow = new google.maps.InfoWindow({
+		content: gilliganString
+	});
+	google.maps.event.addListener(gilliganMarker, 'click', function() {
+		gilliganInfoWindow.open(map,gilliganMarker);
 	});
 	
 };
