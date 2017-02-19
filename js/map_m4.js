7b718b88afcea15cc6843b9350c0784cfd0eb362
Gladys Valentin <gvalentin@unm.edu>
2017-02-18 18:50:56 -0700

    Updated map_m4.js
    Deleted old Google style js section, pasted the new one, added }; at the
    end of that sectiont o make it work, and added infoboxes to all markers.

--- js/map_m4.js
+++ js/map_m4.js
@@ -11,16 +11,27 @@ function initialize() {
 		mapTypeId: google.maps.MapTypeId.HYBRID,
 		styles: [
   {
-    "featureType": "administrative",
-    "elementType": "geometry",
-    "stylers": [
-[
-  {
-    "featureType": "administrative",
-    "elementType": "geometry",
+    "featureType": "administrative.land_parcel",
     "stylers": [
       {
         "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.neighborhood",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.neighborhood",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#a4fdd7"
       }
     ]
   },
@@ -29,7 +40,7 @@ function initialize() {
     "elementType": "geometry.fill",
     "stylers": [
       {
-        "color": "#c3c3c3"
+        "color": "#aaaaaa"
       }
     ]
   },
@@ -38,12 +49,39 @@ function initialize() {
     "elementType": "geometry.fill",
     "stylers": [
       {
-        "color": "#b0e6ce"
+        "color": "#c1ecdb"
       }
     ]
   },
   {
     "featureType": "poi",
+    "elementType": "labels.text",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.business",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#79d0bd"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text",
     "stylers": [
       {
         "visibility": "off"
@@ -52,22 +90,10 @@ function initialize() {
   },
   {
     "featureType": "road",
-    "elementType": "labels.icon",
+    "elementType": "labels",
     "stylers": [
       {
         "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "road.arterial",
-    "elementType": "geometry.fill",
-    "stylers": [
-      {
-        "color": "#ffffff"
-      },
-      {
-        "weight": 1.5
       }
     ]
   },
@@ -76,10 +102,7 @@ function initialize() {
     "elementType": "geometry.fill",
     "stylers": [
       {
-        "color": "#327e92"
-      },
-      {
-        "weight": 1
+        "color": "#000000"
       }
     ]
   },
@@ -88,68 +111,55 @@ function initialize() {
     "elementType": "geometry.stroke",
     "stylers": [
       {
-        "color": "#54c5c5"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "labels.text",
-    "stylers": [
-      {
-        "color": "#0d2626"
-      },
-      {
-        "weight": 1
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#0d2626"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#ffffff"
-      }
-    ]
-  },
-  {
-    "featureType": "transit",
-    "stylers": [
-      {
-        "visibility": "off"
+        "color": "#d7d7d7"
       }
     ]
   },
   {
     "featureType": "water",
-    "elementType": "geometry.fill",
+    "elementType": "labels.text",
     "stylers": [
       {
-        "color": "#89c5d8"
+        "visibility": "off"
       }
     ]
   }
 ]
-
+	};
 	var map = new google.maps.Map(
 		document.getElementById("map_canvas"), 
 		myOptions);
-
+		
+		
+		
+		
+	var playuelaString = 
+		'</div>'+
+        '<div id="bodyContent">'+
+		'<p><b>La Playuela Beach</b>, '+
+        'also known as Playa Sucia, is located ' +
+        'in the municipality of Cabo Rojo, Puerto Rico, near the '+
+        'Cabo Rojo Lighthouse and Las Salinas. '+
+        '</div>'+
+        '</div>';						
+		
+	var playuelaInfoWindow = new google.maps.InfoWindow({
+		content: playuelaString
+		
+	});
+	
 	var playuelaMarker = new google.maps.Marker({
 		position: playuela,
 		title:"La Playuela Beach, Cabo Rojo"
 		});
 	playuelaMarker.setMap(map);
+	
+	playuelaMarker.addListener('click', function() {
+		playuelaInfoWindow.open(map,playuelaMarker);
+	});
+	
+	
+
 	
 	var flamencoMarker = new google.maps.Marker({
 		position: flamenco,
@@ -157,16 +167,41 @@ function initialize() {
 		});
 	flamencoMarker.setMap(map);
 	
+	var flamencoInfoWindow = new google.maps.InfoWindow({
+		content: flamencoInfoWindow
+	});
+	google.maps.event.addListener(flamencoMarker, 'click', function() {
+		flamencoInfoWindow.open(map,flamencoMarker);
+	});
+	
+	
+	
 	var sunbayMarker = new google.maps.Marker({
 		position: sunbay,
 		title:"Sun Bay Beach, Vieques"
 		});
 	sunbayMarker.setMap(map);
 	
+	var sunbayInfoWindow = new google.maps.InfoWindow({
+		content: sunbayInfoWindow
+	});
+	google.maps.event.addListener(sunbayMarker, 'click', function() {
+		sunbayInfoWindow.open(map,sunbayMarker);
+	});
+	
+	
+	
 	var crashboatMarker = new google.maps.Marker({
 		position: crashboat,
 		title:"Crash Boat Beach, Aguadilla"
 		});
 	crashboatMarker.setMap(map);
-
+	
+	var crashboatInfoWindow = new google.maps.InfoWindow({
+		content: crashboatInfoWindow
+	});
+	google.maps.event.addListener(crashboatMarker, 'click', function() {
+		crashboatInfoWindow.open(map,crashboatMarker);
+	});
+	
 };
