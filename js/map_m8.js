09d0158aae60b50eb1d7d4a437a8d8d5ee9ba014
Gladys Valentin <gvalentin@unm.edu>
2017-04-01 11:05:20 -0600

    Edited code for map_m8. Doesn't work.

--- js/map_m8.js
+++ js/map_m8.js
@@ -1,8 +1,28 @@
 var myMap = new ol.Map({
-	target: 'map_canvas"
+	target: 'map_canvas',
+	layers: [
+		new ol.layer.Title({
+			source: new ol.source.OSM()
+		})
+	],
 	view: new ol.View({
 		center: ol.proj.fromLonLant([-109.045187,36.998980]),
 		zoom: 20
 	})
 });
 
+var Layer_Stamen_terrain = new ol.layer.Group({
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.Stamen({layer: 'terrain'})
+        })
+    ]
+});
+
+function setMapType(newType) {
+    if(newType == 'STAMEN_terrain') {
+        map.setLayerGroup(Layer_Stamen_terrain);
+    }
+}
+
+setMapType('STAMEN_terrain')
